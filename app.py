2d9d762c82dcfd91bcb5cec24800e7a63fb0f22f
shinjan20 <70946369+shinjan20@users.noreply.github.com>
2021-12-05 00:31:16 +0530

    second commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import detector
 
 UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
-ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
+ALLOWED_EXTENSIONS = {'jpg', 'png','jpeg'}
 app = Flask(__name__, static_url_path="/static")
 
 app.config['SECRET_KEY'] = 'opencv'  
@@ -20,7 +20,7 @@ app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
+@app.route('/', methods = ["POST", "GET"])
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -34,6 +34,7 @@ def index():
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
             prediction = detector.detection(os.path.join(UPLOAD_FOLDER, filename))
+            print(prediction)
             data = {
                 "uploaded_image" : 'static/uploads/'+filename,
                 "class" : prediction 
