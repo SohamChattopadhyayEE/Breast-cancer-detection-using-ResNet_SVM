12fdf146366230489b01be01aee1c2a2278a3a20
shinjan20 <70946369+shinjan20@users.noreply.github.com>
2021-12-04 22:35:37 +0530

    Add files via upload
    Integrated the frontend with the backend and improved the UI

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,6 @@
-var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
-};
+let image = document.getElementById('output');
+
+let showPreview=function(event){
+		image.src = URL.createObjectURL(event.target.files[0]);
+		image.style.display="block";
+}
